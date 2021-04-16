f6f04461fcebef18ecfb24c0d4c244486eecb3b5
jonahf0 <jonahfitzgerald0113@gmail.com>
2021-04-16 18:43:16 -0500

    separated print functions from main

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,14 @@
 use std::collections::HashMap;
 use std::process::{exit, Command};
 use std::sync::mpsc::channel;
-use std::{fs, thread, time};
-
+use std::{thread, time};
 use clap::{App, Arg};
 
 mod print_results;
+mod read_proc;
 
 use print_results::*;
-
-
-/*reads the /proc files, the first arg is the PID of the process that gets spawned,
-and the second argument is the specific file to read from; currently, it can just accept
-None in order to default to "smaps_rollup"*/
-fn read_from_proc(pid: u32) -> Option<HashMap<String, String>> {
-
-    let proc_file = "smaps_rollup";
-
-    let file_location = format!("/proc/{}/{}", pid.to_string(), proc_file);
-
-    /*used for constructing a map of every line in the file;
-    they all have predictable formats like "Rss: <number> kb"*/
-    let mut return_hash_map: HashMap<String, String> = HashMap::new();
-
-    //main reason for returning an Option -- may be an issue reading from the file
-    let file_contents = match fs::read_to_string(file_location) {
-        Ok(value) => value,
-
-        Err(_) => return None,
-    };
-
-    /*Pretty massive bunch of functions [how I like it >:)] --
-    split the file contents by line, then split by white space into a vector, then
-    remove all ':' chars*/ 
-    let temp = file_contents
-        .split("\n")
-        .filter(|&line| line != "")
-        .map(|line| -> Vec<String> {
-            line.split_whitespace()
-                .collect::<Vec<&str>>()
-                .iter()
-                .map(|line| line.replace(":", ""))
-                .collect()
-        })
-        .collect::<Vec<Vec<String>>>(); 
-
-        //I used for_each() just for fun
-        temp.iter().for_each( |line| {
-
-            /*The key is the initial index of the line, which should actually be a key.
-            In order to account for lines that might have an unexpected format (like of
-            I try reading from "status" instead of "smaps_rollup"), the default "empty"
-            is used.*/
-            return_hash_map.insert(
-                line.get(0).unwrap_or(&"empty".to_string()).to_string(),
-                format!("{} {}", line.get(1).unwrap_or(&"empty".to_string()), line.get(2).unwrap_or(&"empty".to_string())),
-                );
-
-        });
-    
-    return Some(return_hash_map);
-}
+use read_proc::*;
 
 fn run_process(executable: &str, exec_args: Vec<&str>, interval: (u64, &str)) {
 
@@ -106,7 +54,7 @@ fn run_process(executable: &str, exec_args: Vec<&str>, interval: (u64, &str)) {
         true
     } {
 
-        let mem_info = read_from_proc(pid);
+        let mem_info = read_smaps_rollup(pid);
 
         match mem_info {
             Some(map) => mem_info_list.push(map),
@@ -142,7 +90,8 @@ fn main() {
     .get_matches();
 
     /*just a variable for keeping track of how many ms to wait before reading smaps;
-    May be an optional cmd arg in the future*/
+    May be an optional cmd arg in the future -- 
+    is a tuple representing a u64 of the actual value and the a str of the unit*/
     let interval: (u64, &str) = match matches.values_of("interval") {
 
         Some(vals) => {
@@ -183,6 +132,7 @@ fn main() {
 
     };
 
+    //represents the thing to execute and observe memory usage of
     let executable = match matches.value_of("executable") {
 
         Some(val) => val,
@@ -197,6 +147,7 @@ fn main() {
 
     };
 
+    //args to pass to the wrapped executable
     let exec_args = match matches.values_of("executable args") {
 
         Some(vals) => vals.collect::<Vec<&str>>().clone(),
