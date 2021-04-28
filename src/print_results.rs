61dac07edaf147d0d0f803928c751d345573a2b4
jonahf0 <jonahfitzgerald0113@gmail.com>
2021-04-28 11:18:02 -0500

    added a function to write to a file instead of printing output, currently  commented out

--- src/print_results.rs
+++ src/print_results.rs
@@ -29,6 +29,37 @@ pub fn print_smaps_result(mem_info: BTreeMap<u64, HashMap<String, String>>, unit
     }
 }
 
+/*
+pub fn write_to_file(mem_info: BTreeMap<u64, HashMap<String, String>>, location: &str) {
+    //previous_data acts as a way to compare when the memory usage changes
+    let mut previous_data = "".to_string();
+
+    for (key, map) in mem_info {
+        let current_data = format!(
+            "Resident set size: {}\nProportional set size: {}\n",
+            map["Rss"], map["Pss"],
+        );
+
+        //prints the data and puts current into previous if the previous data does not
+        //equal the current (i.e. a change occured in smaps_rollup);
+        //I could have used an if-else statement, but meh
+        match previous_data == current_data {
+            true => {}
+
+            false => {
+
+                print_formatted_time(key, );
+
+                println!("{}", current_data);
+
+                previous_data = current_data;
+            }
+        }
+    }
+
+}
+*/
+
 fn print_formatted_time(key: u64, unit: &str) {
 
     let time_passed = match unit {
